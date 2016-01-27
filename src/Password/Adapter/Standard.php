b423d418d811e24a6c2f90314d3af5cab5b14573
matt <matty201@gmail.com>
2016-01-27 14:10:36 +0100

    change docs

--- src/Password/Adapter/Standard.php
+++ src/Password/Adapter/Standard.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of Vegas package
  *
- * @author Slawomir Zytko <slawomir.zytko@gmail.com>
+ * @author Slawomir Zytko <slawek@amsterdam-standard.pl>
  * @copyright Amsterdam Standard Sp. Z o.o.
  * @homepage http://vegas-cmf.github.io
  *
@@ -20,14 +20,14 @@ use \Vegas\Security\Password\PasswordInterface;
  */
 class Standard implements PasswordInterface
 {
-    /** @var \Phalcon\DI $di  */
+    /** @var \Phalcon\DiInterface $di */
     protected $di;
 
     /**
      * Standard constructor.
-     * @param \Phalcon\DI $di
+     * @param \Phalcon\DiInterface $di
      */
-    public function __construct(\Phalcon\DI $di)
+    public function __construct(\Phalcon\DiInterface $di)
     {
         $this->di = $di;
     }
